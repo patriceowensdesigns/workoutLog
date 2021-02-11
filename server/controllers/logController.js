f3af756c9bb718778fee5c94097a07f37bdbe83a
patriceowensdesigns <patriceowensdesigns@gmail.com>
2021-02-11 04:08:40 -0500

    log.js code

--- server/controllers/logController.js
+++ server/controllers/logController.js
@@ -1,10 +1,24 @@
 let express = require('express');
 let router = express.Router();
 let validateSession = require('../middleware/validate-session');
+const Log = require('../db').import('../models/log');
 
 router.get('/practice', validateSession, function(req, res)
 {
     res.send ('Hey!! This is a practice route!')
-})
+});
+
+router.post('/create', validateSession, (req, res) => {
+    const logEntry = {
+        description: req.body.log.description, 
+        definition: req.body.log.definition,
+        result: req.body.log.result, 
+        owner_id: req.user.id
+    }
+    Log.create(logEntry)
+    .then(log => res.status(200).json(log))
+    .catch(err => res.status(500).json({error: err}))
+});
+
 
 module.exports = router;
