b44efc7fc909f76aaa0c78b7b3ac8a4ba6e83f7b
patriceowensdesigns <patriceowensdesigns@gmail.com>
2021-04-07 23:21:13 -0400

    WorkoutCreate, WorkoutTable and WorkoutEdit added to workouts folder which were not committed upon completion

--- client/src/workouts/WorkoutIndex.js
+++ client/src/workouts/WorkoutIndex.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {Container, Row, Col} from 'reactstrap';
+import WorkoutCreate from './WorkoutCreate';
+import WorkoutTable from './WorkoutTable';
+import WorkoutEdit from './WorkoutEdit';
 
 const WorkoutIndex = (props) => {
     const [workouts, setWorkouts] = useState([]);
+    const [updateActive, setUpdateActive] = useState(false);
+    const [workoutToUpdate, setWorkoutToUpdate] = useState({});
+
     const fetchWorkouts = () => {
-        fetch('http://localhost:3000/log', {
+        fetch('http://localhost:3000/log/mine', {
             method: 'GET',
             headers: new Headers ({
                 'Content-Type': 'application/json',
@@ -17,6 +23,19 @@ const WorkoutIndex = (props) => {
            }) 
     }
 
+    const editUpdateWorkout = (workout) => {
+        setWorkoutToUpdate(workout);
+        console.log(workout);
+    }
+
+    const updateOn = () => {
+        setUpdateActive(true);
+    }
+
+    const updateOff = () => {
+        setUpdateActive(false);
+    }
+
     useEffect(() => {
         fetchWorkouts();
     }, []);
@@ -24,12 +43,13 @@ const WorkoutIndex = (props) => {
     return(
         <Container>
             <Row>
-                <Col med="3">
-                    {/*The create component will go here */}
+                <Col md="3">
+                    <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
                 </Col>
-                <Col med="9">
-                    <h2>Log a workout to see a table. This will be added in later pages.</h2>
+                <Col md="9">
+                    <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token} />
                 </Col>
+                {updateActive ? <WorkoutEdit workoutToUpdate={workoutToUpdate}  updateOff={updateOff} token={props.token} fetchWorkouts={fetchWorkouts}/> : <></> }
             </Row>
         </Container>
     )
